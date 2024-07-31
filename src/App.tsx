84ebb2d090d672f7b58247f5ef05e8df0a32b0da
Islomjon6975 <islomabdulakhatov6975@gmail.com>
2024-07-31 16:21:36 +0500

    feat(input): added input component

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import Button from '@generic/button';
+import Input from '@generic/input';
 import Theme from '@tools/theme';
 
 const App = () => {
@@ -6,13 +7,16 @@ const App = () => {
   return (
     <div className="app">
       <Theme />
+      <h1>Buttons</h1>
       <Button variant='primary'>Primary</Button>
       <Button variant='secondary'>Secondary</Button>
       <Button variant='default'>Default</Button>
       <Button variant="text">Text</Button>
       <Button variant="link">Link</Button>
-      <h1 className='text-black bg-white'>Title</h1>
-
+      <h1 className='text-black bg-white'>Inputs</h1>
+      <Input size='small' placeholder='Search...' />
+      <Input size='medium' placeholder='Search...' />
+      <Input size='large' placeholder='Search...' />
     </div>
   );
 };
---
+++ src/generic/input/index.tsx
@@ -0,0 +1,37 @@
+import { Input as InputComp, InputRef } from "antd";
+import { forwardRef, memo, ForwardedRef, FC } from "react";
+import { InputPropI } from "@interfaces/generic";
+import { inputStyle } from "./style";
+
+const Input:FC = memo(
+  forwardRef<InputRef, InputPropI>(
+    (
+      { type = "text", placeholder, size = "medium", className, ...props },
+      ref: ForwardedRef<InputRef>
+    ) => {
+
+      if (type === "password") {
+        return (
+          <InputComp.Password
+            ref={ref}
+            placeholder={placeholder}
+            className={inputStyle({size, className})}
+            {...props}
+          />
+        );
+      }
+
+      return (
+        <InputComp
+          ref={ref}
+          type={type}
+          placeholder={placeholder}
+          className={inputStyle({size, className})}
+          {...props}
+        />
+      );
+    }
+  )
+);
+
+export default Input;
